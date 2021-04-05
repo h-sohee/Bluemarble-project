e3fa268fa01b88667a8d20738f12f6576b25c995
sh6863 <kristen6863@gmail.com>
2021-04-05 18:24:53 +0900

    부루마블 게임 프로젝트

--- blueMarble/drawScreen.py
+++ blueMarble/drawScreen.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pygame
 import sys
 
@@ -36,6 +34,14 @@ p2Img = pygame.image.load("images/p2.png")
 p3Img = pygame.image.load("images/p3.png")
 p4Img = pygame.image.load("images/p4.png")
 
+mapBack = pygame.image.load("images/mapBack.png")
+clickedMap = pygame.image.load("images/clickedMap.png")
+
+yes = pygame.image.load("images/yes.png")
+clickedYes = pygame.image.load("images/clickedYes.png")
+no = pygame.image.load("images/no.png")
+clickedNo = pygame.image.load("images/clickedNo.png")
+
 clock = pygame.time.Clock()
 pygame.display.set_caption("Blue Marble")
 
@@ -47,8 +53,10 @@ textAlignBlock = 3
 pygame.font.init()
 
 font = pygame.font.SysFont("malgungothicsemilight", 15, True, False)
+pColor = [RED, YELLOW, BLUE, GREEN]
 
 
+# 액션버튼 생성
 def createButton(img_in, x, y, width, height, img_act, x_act, y_act):
     mPos = pygame.mouse.get_pos()
     if x + width > mPos[0] > x and y + height > mPos[1] > y:
@@ -57,6 +65,7 @@ def createButton(img_in, x, y, width, height, img_act, x_act, y_act):
         SCREEN.blit(img_in, (x, y))
 
 
+# 화면에 텍스트 표시
 def writeText(s, size, c, x, y):
     f = pygame.font.SysFont("malgungothicsemilight", size, True, False)
     textTitle = f.render(s, True, c)
@@ -121,7 +130,7 @@ def drawText(text, color, rect, align=textAlignLeft, bkg=None):
 
 
 # 플레이어 그리기
-def drawPlayer(cp, pl, pColor):
+def drawPlayer(cp, pl):
     for i in range(1, cp + 1):
         if i == 1:
             pygame.draw.circle(SCREEN, pColor[i - 1], [650, 80], 20)
@@ -133,7 +142,7 @@ def drawPlayer(cp, pl, pColor):
 
             textRect = pygame.Rect(710, 80, 200, 80)
             drawTextRect = textRect.inflate(-5, -5)
-            drawText(str(pl[i - 1]['land']), (0, 0, 0), drawTextRect, textAlignBlock)
+            drawText(str(pl[i - 1]['land']), (0, 0, 0), drawTextRect)
         else:
             pygame.draw.circle(SCREEN, pColor[i - 1], [650, 130 * i - 50], 20)
             pygame.draw.rect(SCREEN, pColor[i - 1], [650, 130 * i - 50, 300, 80], 1)
@@ -144,14 +153,13 @@ def drawPlayer(cp, pl, pColor):
 
             textRect = pygame.Rect(710, 130 * i - 50, 200, 80)
             drawTextRect = textRect.inflate(-5, -5)
-            drawText(str(pl[i - 1]['land']), (0, 0, 0), drawTextRect, textAlignBlock)
+            drawText(str(pl[i - 1]['land']), (0, 0, 0), drawTextRect)
 
         pygame.draw.circle(SCREEN, pColor[i - 1], [pl[i - 1]['position'][0] + ((i - 1) * 20), pl[i - 1]['position'][1]], 8)
 
 
 # 플레이 화면
 def drawMap(count, cp, pl):
-    pColor = [RED, YELLOW, BLUE, GREEN]
     SCREEN.fill(WHITE)
 
     count += 1
@@ -161,9 +169,161 @@ def drawMap(count, cp, pl):
     SCREEN.blit(mapImg, (80, 50))
 
     # 주사위 굴리기 버튼
-    createButton(rollImg, 370, 350, 100, 100, clickRollImg, 370, 350)
+    createButton(rollImg, 380, 350, 100, 100, clickRollImg, 370, 350)
 
-    drawPlayer(cp, pl, pColor)
+    drawPlayer(cp, pl)
+
+
+# yes 메시지 출력
+def yMessageShow(msg, count, cp, pl):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_pos = pygame.mouse.get_pos()
+                if 300 < mouse_pos[0] < 385 and 250 < mouse_pos[1] < 280:
+                    return True
+
+        SCREEN.fill(WHITE)
+
+        writeText("현재 순서: 플레이어 " + str(count % cp + 1), 20, pColor[count % cp], 10, 10)
+
+        SCREEN.blit(mapImg, (80, 50))
+
+        textRect = pygame.Rect(200, 150, 300, 100)
+        drawTextRect = textRect.inflate(-5, -5)
+        drawText(msg, (0, 0, 0), drawTextRect)
+
+        createButton(yes, 300, 250, 85, 30, clickedYes, 300, 250)
+
+        drawPlayer(cp, pl)
+
+        pygame.display.update()
+        clock.tick(15)
+
+
+# yes/no 메시지 출력
+def ynMessageShow(msg, count, cp, pl):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_pos = pygame.mouse.get_pos()
+                if 300 < mouse_pos[0] < 385 and 250 < mouse_pos[1] < 280:
+                    return True
+                elif 400 < mouse_pos[0] < 485 and 250 < mouse_pos[1] < 280:
+                    return False
+
+        SCREEN.fill(WHITE)
+
+        writeText("현재 순서: 플레이어 " + str(count % cp + 1), 20, pColor[count % cp], 10, 10)
+
+        SCREEN.blit(mapImg, (80, 50))
+
+        textRect = pygame.Rect(200, 150, 300, 100)
+        drawTextRect = textRect.inflate(-5, -5)
+        drawText(msg, (0, 0, 0), drawTextRect)
+
+        createButton(yes, 300, 250, 85, 30, clickedYes, 300, 250)
+        createButton(no, 400, 250, 85, 30, clickedNo, 400, 250)
+
+        drawPlayer(cp, pl)
+
+        pygame.display.update()
+        clock.tick(15)
+
+
+# 도시클릭
+def cityClicked(count, cp, pl):
+    click = True
+
+    while click:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_pos = pygame.mouse.get_pos()
+                # return 도시번호
+                if 505 < mouse_pos[0] < 590 and 135 < mouse_pos[1] < 220:
+                    return 16
+                elif 505 < mouse_pos[0] < 590 and 220 < mouse_pos[1] < 305:
+                    return 17
+                elif 505 < mouse_pos[0] < 590 and 305 < mouse_pos[1] < 390:
+                    return 18
+                elif 505 < mouse_pos[0] < 590 and 390 < mouse_pos[1] < 475:
+                    return 19
+                elif 505 < mouse_pos[0] < 590 and 475 < mouse_pos[1] < 560:
+                    return 0
+                elif 420 < mouse_pos[0] < 505 and 475 < mouse_pos[1] < 560:
+                    return 1
+                elif 335 < mouse_pos[0] < 420 and 475 < mouse_pos[1] < 560:
+                    return 2
+                elif 250 < mouse_pos[0] < 335 and 475 < mouse_pos[1] < 560:
+                    return 3
+                elif 165 < mouse_pos[0] < 250 and 475 < mouse_pos[1] < 560:
+                    return 4
+                elif 80 < mouse_pos[0] < 165 and 475 < mouse_pos[1] < 560:
+                    return 5
+                elif 80 < mouse_pos[0] < 165 and 390 < mouse_pos[1] < 475:
+                    return 6
+                elif 80 < mouse_pos[0] < 165 and 305 < mouse_pos[1] < 390:
+                    return 7
+                elif 80 < mouse_pos[0] < 165 and 220 < mouse_pos[1] < 305:
+                    return 8
+                elif 80 < mouse_pos[0] < 165 and 135 < mouse_pos[1] < 220:
+                    return 9
+                elif 80 < mouse_pos[0] < 165 and 50 < mouse_pos[1] < 135:
+                    return 10
+                elif 165 < mouse_pos[0] < 250 and 50 < mouse_pos[1] < 135:
+                    return 11
+                elif 250 < mouse_pos[0] < 335 and 50 < mouse_pos[1] < 135:
+                    return 12
+                elif 335 < mouse_pos[0] < 420 and 50 < mouse_pos[1] < 135:
+                    return 13
+                elif 420 < mouse_pos[0] < 505 and 50 < mouse_pos[1] < 135:
+                    return 14
+
+        SCREEN.fill(WHITE)
+
+        writeText("현재 순서: 플레이어 " + str(count % cp + 1), 20, pColor[count % cp], 10, 10)
+
+        # 도시 표시
+        createButton(mapBack, 505, 135, 85, 85, clickedMap, 505, 135)     # 런던
+        createButton(mapBack, 505, 220, 85, 85, clickedMap, 505, 220)     # 뉴욕
+        createButton(mapBack, 505, 305, 85, 85, clickedMap, 505, 305)     # 접수처
+        createButton(mapBack, 505, 390, 85, 85, clickedMap, 505, 390)     # 서울
+        createButton(mapBack, 505, 475, 85, 85, clickedMap, 505, 475)     # 출발
+        createButton(mapBack, 420, 475, 85, 85, clickedMap, 420, 475)     # 타이베이
+        createButton(mapBack, 335, 475, 85, 85, clickedMap, 335, 475)     # 싱가포르
+        createButton(mapBack, 250, 475, 85, 85, clickedMap, 250, 475)     # 카이로
+        createButton(mapBack, 165, 475, 85, 85, clickedMap, 165, 475)     # 이스탄불
+        createButton(mapBack, 80, 475, 85, 85, clickedMap, 80, 475)       # 무인도
+        createButton(mapBack, 80, 390, 85, 85, clickedMap, 80, 390)       # 아테네
+        createButton(mapBack, 80, 305, 85, 85, clickedMap, 80, 305)       # 코펜하겐
+        createButton(mapBack, 80, 220, 85, 85, clickedMap, 80, 220)       # 황금카드
+        createButton(mapBack, 80, 135, 85, 85, clickedMap, 80, 135)       # 오타와
+        createButton(mapBack, 80, 50, 85, 85, clickedMap, 80, 50)         # 사회복지
+        createButton(mapBack, 165, 50, 85, 85, clickedMap, 165, 50)       # 상파울루
+        createButton(mapBack, 250, 50, 85, 85, clickedMap, 250, 50)       # 시드니
+        createButton(mapBack, 335, 50, 85, 85, clickedMap, 335, 50)       # 리스본
+        createButton(mapBack, 420, 50, 85, 85, clickedMap, 420, 50)       # 마드리드
+
+        # 맵 이미지 출력
+        SCREEN.blit(mapImg, (80, 50))
+
+        textRect = pygame.Rect(180, 150, 310, 150)
+        drawTextRect = textRect.inflate(-5, -5)
+        drawText("당신은 우주여행에 도착했습니다. 다음 턴에 원하는 장소로 이동할 수 있습니다. 이동을 원하는 도시를 지도에서 선택하세요.", (0, 0, 0), drawTextRect)
+
+        drawPlayer(cp, pl)
+
+        pygame.display.update()
+        clock.tick(15)
 
 
 # 시작화면
@@ -212,6 +372,7 @@ def countPlayer():
         SCREEN.fill(WHITE)
 
         SCREEN.blit(infoImg, (100, 100))
+
         # 인원 수 버튼
         createButton(twoImg, 200, 300, 132, 141, clickTwoImg, 200, 300)
         createButton(threeImg, 400, 300, 132, 141, clickThreeImg, 400, 300)
--- blueMarble/main.py
+++ blueMarble/main.py
@@ -1,34 +1,37 @@
 import drawScreen
 import pygame
 import random
-import tkinter
-from tkinter import ttk
 from tkinter import *
 from tkinter import messagebox
 
-# 도시 건물가격 설정하기
-# 우주여행 이벤트
-# 게임 종료 조건 -> 파산/턴 횟수
-
 clock = pygame.time.Clock()
+
+# 도시이름
 land = ['출발', '타이베이', '싱가포르', '카이로', '이스탄불',
-        '무인도', '아테네', '코펜하겐', '베를린', '오타와',
+        '무인도', '아테네', '코펜하겐', '황금카드', '오타와',
         '사회복지', '상파울루', '시드니', '리스본', '마드리드',
         '우주여행', '런던', '뉴욕', '접수처', '서울']
-pPos = [[510, 535], [426, 535], [342, 535], [259, 535], [175, 535],
-        [91, 535], [91, 452], [91, 370], [91, 287], [91, 204],
-        [91, 122], [175, 122], [259, 122], [342, 122], [426, 122],
-        [510, 122], [510, 204], [510, 287], [510, 370], [510, 452]]
-# [도시가격, 통행료, 건물비]
-landMoney = [0, [100000, 20000, 50000], [100000, 30000, 50000], [100000, 40000, 50000], [100000, 50000, 50000],
-             0, [200000, 30000, 50000], [200000, 40000, 50000], [200000, 50000, 50000], [200000, 60000, 50000],
-             0, [300000, 40000, 50000], [300000, 50000, 50000], [300000, 60000, 50000], [300000, 70000, 50000],
-             0, [400000, 50000, 50000], [400000, 60000, 50000], 0, [400000, 100000, 50000]]
+# 도시별 좌표
+pPos = [[517, 545], [432, 545], [347, 545], [262, 545], [177, 545],
+        [92, 545], [92, 460], [92, 375], [92, 290], [92, 205],
+        [92, 120], [177, 120], [262, 120], [347, 120], [432, 120],
+        [517, 120], [517, 205], [517, 290], [517, 375], [517, 460]]
+# [도시가격, 통행료, 건물가격, 건물개수]
+landMoney = [0, [50000, 20000, 10000, 0], [80000, 40000, 20000, 0], [100000, 60000, 30000, 0], [120000, 80000, 40000, 0],
+             0, [140000, 100000, 50000, 0], [160000, 120000, 60000, 0], 0, [200000, 160000, 80000, 0],
+             0, [220000, 180000, 90000, 0], [240000, 220000, 110000, 0], [260000, 220000, 110000, 0], [280000, 240000, 120000, 0],
+             0, [300000, 260000, 130000, 0], [320000, 280000, 150000, 0], 0, [350000, 350000, 170000, 0]]
+# 황금카드
+goldCard = ["병원비 지불: 병원에서 건강진단을 받았습니다. 병원비 10000원을 납부합니다.",
+            "복권당첨: 복권에 당첨되었습니다. 당첨금 200000원을 받습니다.",
+            "이사: 뒤로 두칸 이동하세요.",
+            "고속도로: 출발지까지 이동해서 월급을 받습니다.",
+            "생일 축하: 모두에게 생일축하를 받으세요. 전원에게 축하금 10000원씩 받습니다."]
 
 
 def main():
     pygame.init()
-    # drawScreen.mainMenu()
+    drawScreen.mainMenu()
     cp = drawScreen.countPlayer()
     playGame(cp)
 
@@ -38,7 +41,7 @@ def makePlayer(cp):
     pList = []
 
     for x in range(1, cp + 1):
-        player = {'position': pPos[0], 'money': 1000000, 'land': [], 'building': {}, 'island': 0, 'space': False, 'live': True}
+        player = {'position': pPos[0], 'money': 1000000, 'land': [], 'event': 0, 'space': 15, 'live': True}
         pList.append(player)
 
     return pList
@@ -48,24 +51,35 @@ def makePlayer(cp):
 def rollDice(playerList, count, cp):
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
-    print(dice1, dice2)
-    
-    # 무인도 탈출조건
-    if playerList[count % cp]['island'] > 0:
-        if dice1 == dice2 or playerList[count % cp]['island'] > 3:
-            playerList[count % cp]['island'] = 0
-            dice = dice1, dice2
-        else:
-            dice = 0, 0
-    elif playerList[count % cp]['island'] == 0:
-        dice = dice1, dice2
+    move = 0
 
-    return dice, dice1, dice2
+    # 무인도 탈출조건
+    if 0 < playerList[count % cp]['event'] < 5:
+        if dice1 == dice2 or playerList[count % cp]['event'] == 4:
+            playerList[count % cp]['event'] = 0
+            move = dice1 + dice2
+        else:
+            move = 0
+    # 우주여행
+    elif playerList[count % cp]['event'] == 5:
+        move = 0
+    elif playerList[count % cp]['event'] == 6:
+        choice = playerList[count % cp]['space']
+
+        if choice - 15 < 0:
+            move = 20 + choice - 15
+        else:
+            move = choice - 15
+
+        playerList[count % cp]['event'] = 0
+    elif playerList[count % cp]['event'] == 0:
+        move = dice1 + dice2
+
+    return move, dice1, dice2
 
 
 # 플레이어 이동
-def movePlayer(dice, count, cp, pList, playerList):
-    move = dice[0] + dice[1]
+def movePlayer(move, count, cp, pList, playerList):
     pList[count % cp] = pList[count % cp] + move
 
     # 월급
@@ -73,6 +87,7 @@ def movePlayer(dice, count, cp, pList, playerList):
         playerList[count % cp]['money'] += 200000
         pList[count % cp] = pList[count % cp] - 20
 
+    print(count % cp, move, land[pList[count % cp]])
     return pList[count % cp]
 
 
@@ -100,7 +115,7 @@ def checkWinner(cp, playerList):
     temp.sort()
     print(temp)
 
-    return temp[cp-1][1]
+    return temp[cp - 1][1]
 
 
 # 게임 시작
@@ -132,85 +147,148 @@ def playGame(cp):
                     diceFlagged = True
                     count = count + 1
 
+        msg = ""
+        if playerList[count % cp]['event'] == 5:
+            playerList[count % cp]['event'] = 6
+
         # 맵그리기
         drawScreen.drawMap(count, cp, playerList)
 
         # 턴 횟수 종료
-        if count == cp * 10:
+        if count == cp * 20:
             w = checkWinner(cp, playerList)
-            msg = '게임이 종료되었습니다. 우승자는 플레이어 ' + str(w + 1) + '입니다.\n게임을 새로 시작하시겠습니까?'
-            if messagebox.askyesno('종료', msg):
+            msg = '게임이 종료되었습니다. 우승자는 플레이어 ' + str(w + 1) + '입니다. 게임을 새로 시작하시겠습니까?'
+            choose = drawScreen.ynMessageShow(msg, count, cp, playerList)
+            if choose:
                 main()
             else:
                 break
 
         if diceFlagged:
-            dice, dice1, dice2 = rollDice(playerList, count, cp)
-            nPos = movePlayer(dice, count, cp, pList, playerList)
+            if playerList[count % cp]['event'] == 5:
+                playerList[count % cp]['event'] += 1
+
+            move, dice1, dice2 = rollDice(playerList, count, cp)
+            nPos = movePlayer(move, count, cp, pList, playerList)
             playerList[count % cp]['position'] = pPos[nPos]
-            
+
             check, p = checkLand(cp, playerList, nPos)
 
+            # 주인 없는 땅에 도착
             if not check:
+                d = dice1, dice2
                 # 무인도
                 if land[nPos] == land[5]:
-                    m = dice1, dice2
-                    msg = str(m) + '가 나왔습니다.\n당신은 무인도에 갖혔습니다.\n' + str(3 - playerList[count % cp]['island']) + '턴이 지나거나 주사위를 던져 같은 수가 나오게 하세요.'
-                    messagebox.showinfo('무인도', msg)
-                    playerList[count % cp]['island'] += 1
+                    msg = str(d) + '가 나왔습니다.당신은 무인도에 갖혔습니다.' + str(
+                        3 - playerList[count % cp]['event']) + '턴이 지나거나 주사위를 던져 같은 수가 나오게 하세요.'
+                    drawScreen.yMessageShow(msg, count, cp, playerList)
+                    playerList[count % cp]['event'] += 1
                 # 출발 위치
                 elif land[nPos] == land[0]:
                     pass
                 # 사회복지
                 elif land[nPos] == land[10]:
-                    msg = str(dice) + '가 나왔습니다.\n당신은 사회복지기금으로 100000원을 지불해야합니다.'
-                    messagebox.showinfo('사회복지기금', msg)
+                    msg = str(d) + '가 나왔습니다.당신은 사회복지기금으로 100000원을 지불해야합니다.'
+                    drawScreen.yMessageShow(msg, count, cp, playerList)
                     if playerList[count % cp]['money'] < 100000:
                         msg = '당신은 지불할 돈이 부족합니다.'
                         messagebox.showinfo('사회복지기금-돈 부족', msg)
-                        pass
                     else:
                         playerList[count % cp]['money'] -= 100000
                         landMoney[18] += 100000
                 # 접수처
                 elif land[nPos] == land[18]:
-                    msg = str(dice) + '가 나왔습니다.\n당신은 사회복지기금으로 모인 ' + str(landMoney[18]) + '원을 받았습니다.'
-                    messagebox.showinfo('접수처', msg)
+                    msg = str(d) + '가 나왔습니다.당신은 사회복지기금으로 모인 ' + str(landMoney[18]) + '원을 받았습니다.'
+                    drawScreen.yMessageShow(msg, count, cp, playerList)
                     playerList[count % cp]['money'] += landMoney[18]
                     landMoney[18] = 0
-                # 우주여행 -> 어떻게 선택???
+                # 우주여행
                 elif land[nPos] == land[15]:
-                    msg = str(dice) + '가 나왔습니다.\n당신은 다음 턴에 원하는 장소로 이동할 수 있습니다. 어디로 이동하시겠습니까?'
-                    messagebox.showinfo('우주여행', msg)
+                    space = drawScreen.cityClicked(count, cp, playerList)
+                    playerList[count % cp]['space'] = space
+                    playerList[count % cp]['event'] = 5
+                # 황금카드
+                elif land[nPos] == land[8]:
+                    card = random.randint(0, 4)
+                    msg = str(d) + '가 나왔습니다.' + goldCard[card]
+                    choose = drawScreen.yMessageShow(msg, count, cp, playerList)
+                    if choose:
+                        if card == 0:
+                            playerList[count % cp]['money'] -= 10000
+                        elif card == 1:
+                            playerList[count % cp]['money'] += 200000
+                        elif card == 2:
+                            nPos = movePlayer(-2, count, cp, pList, playerList)
+                            playerList[count % cp]['position'] = pPos[nPos]
+                        elif card == 3:
+                            nPos = movePlayer(12, count, cp, pList, playerList)
+                            playerList[count % cp]['position'] = pPos[nPos]
+                        elif card == 4:
+                            for i in range(len(playerList)):
+                                if i == count % cp:
+                                    playerList[i]['money'] += 10000 * (len(playerList) - 1)
+                                else:
+                                    playerList[i]['money'] -= 10000
+
                 # 구매
                 else:
-                    msg = str(dice) + '가 나왔습니다.\n' + str(land[nPos]) + ' 땅은 ' + str(landMoney[nPos][0]) + '원입니다. 땅을 사겠습니까?'
-                    if messagebox.askyesno('땅 구매', msg):
+                    msg = str(d) + '가 나왔습니다.' + str(land[nPos]) + ' 땅은 ' + str(
+                        landMoney[nPos][0]) + '원입니다. 땅을 사겠습니까?'
+                    choose = drawScreen.yMessageShow(msg, count, cp, playerList)
+                    if choose:
                         if playerList[count % cp]['money'] < landMoney[nPos][0]:
-                            msg = '당신은 지불할 돈이 부족합니다.'
-                            messagebox.showinfo('땅 구매-돈 부족', msg)
-                            pass
+                            msg = '당신은 지불할 돈이 부족합니다. 땅을 구매할 수 없습니다.'
+                            drawScreen.yMessageShow(msg, count, cp, playerList)
                         else:
                             playerList[count % cp]['land'].append(land[nPos])
                             playerList[count % cp]['money'] -= landMoney[nPos][0]
+            # 주인 있는 땅에 도착
             else:
-                # 통행료
+                # 다른 플레이어 도시에 도착 - 통행료
                 if p != (count % cp):
-                    # 도시 판매
+                    msg = str(d) + ' 가 나왔습니다.' + str(land[nPos]) + ' 땅은 플레이어 ' + str(p + 1) + '의 도시입니다. 통행료 ' + str(landMoney[nPos][1]) + '원을 내세요.'
+                    drawScreen.yMessageShow(msg, count, cp, playerList)
+                    # 돈 부족
                     if playerList[count % cp]['money'] < landMoney[nPos][1]:
-                        playerList[count % cp]['live'] = False
-                        # del playerList[count % cp]['land']
+                        msg = '돈이 부족합니다. 소유한 땅을 판매하세요.'
+                        drawScreen.yMessageShow(msg, count, cp, playerList)
+                        # 도시 판매 불가 - 파산
+                        if len(playerList[count % cp]['land']) == 0:
+                            msg = '판매할 땅이 없습니다. 파산입니다.'
+                            drawScreen.yMessageShow(msg, count, cp, playerList)
+                            playerList[count % cp]['live'] = False
+                        # 도시 판매
+                        else:
+                            msg = playerList[count % cp]['land'][0] + '땅을 판매합니다. (제일 먼저 구매한 땅)'
+                            drawScreen.yMessageShow(msg, count, cp, playerList)
 
-                    msg = str(dice) + ' 가 나왔습니다.\n' + str(land[nPos]) + ' 땅은 플레이어 ' + str(p + 1) + '의 도시입니다. 통행료 ' + str(landMoney[nPos][1]) + '원을 내세요.'
-                    messagebox.showinfo('통행료', msg)
-                    playerList[count % cp]['money'] -= landMoney[nPos][1]
-                    playerList[p]['money'] += landMoney[nPos][1]
-                # 건물 생성
+                            for i in range(len(land)):
+                                if land[i] == playerList[count % cp]['land'][0]:
+                                    playerList[count % cp]['money'] += int(
+                                        (landMoney[i][0] + landMoney[i][2] * landMoney[i][3]) / 2)
+                                    if playerList[count % cp]['money'] >= landMoney[nPos][1]:
+                                        break
+
+                            del playerList[count % cp]['land'][0]
+                            playerList[p]['money'] += landMoney[nPos][1]
+                    else:
+                        playerList[count % cp]['money'] -= landMoney[nPos][1]
+                        playerList[p]['money'] += landMoney[nPos][1]
+                # 본인 땅에 도착 - 건물 생성
                 else:
-                    print("플레이어 ", p, count % cp)
-                    msg = str(dice) + ' 가 나왔습니다.\n' + str(land[nPos]) + ' 땅은 당신의 도시입니다. 건물을 지으시겠습니까?\n건물은 ' + str(landMoney[nPos][2]) + '원입니다.'
-                    messagebox.showinfo('건물 생성', msg)
-                    playerList[count % cp]['money'] -= landMoney[nPos][2]
+                    msg = str(d) + ' 가 나왔습니다.' + str(land[nPos]) + ' 땅은 당신의 도시입니다. 건물을 지으시겠습니까? 건물생성 비용은 ' + str(
+                        landMoney[nPos][2]) + '원입니다.'
+                    choose = drawScreen.ynMessageShow(msg, count, cp, playerList)
+                    if choose:
+                        if playerList[count % cp]['money'] < landMoney[nPos][0]:
+                            msg = '당신은 지불할 돈이 부족합니다. 건물을 생성할 수 없습니다.'
+                            drawScreen.yMessageShow(msg, count, cp, playerList)
+                        else:
+                            msg = "건물을 지었습니다. 도시의 통행료가 증가했습니다."
+                            drawScreen.yMessageShow(msg, count, cp, playerList)
+                            playerList[count % cp]['money'] -= landMoney[nPos][2]
+                            landMoney[nPos][3] += 1
+                            landMoney[nPos][1] += landMoney[nPos][2] * landMoney[nPos][3]
             diceFlagged = False
 
         pygame.display.flip()
